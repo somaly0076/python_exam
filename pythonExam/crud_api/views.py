a6a48e66957e90dc2a7de0288810f3113759f21c
somaly0076 <raksmeysomalyek0076@gmail.com>
2025-01-29 13:37:43 +0700

    created search filter by id, name and image

--- pythonExam/crud_api/views.py
+++ pythonExam/crud_api/views.py
@@ -15,15 +15,41 @@ def create_product(request):
     return Response(serializer.errors,status= status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET'])
-def get_product(request):
+def get_product(request,*args,**kwargs):
     products = ProductTB.objects.all()
-    prod_serializer = ProductSerializer(products,many = True)
-    return Response(prod_serializer.data)
+    search_id = request.query_params.get('id',None)
+    search_name = request.query_params.get('name',None)
+    search_image = request.query_params.get('image',None)
+    if search_id:
+        product = products.filter(id=search_id)
+        prod_serializer = ProductSerializer(product,many = True)
+        return Response(prod_serializer.data)
+    elif search_name:
+        product = products.filter(name__contains=search_name)
+        prod_serializer = ProductSerializer(product,many = True)
+        return Response(prod_serializer.data)
+    elif search_image:
+        product = products.filter(image=search_image)
+        prod_serializer = ProductSerializer(product,many = True)
+
+    prods_serializer = ProductSerializer(products,many = True)
+    return Response(prods_serializer.data)
+
 
 
 @api_view(['GET'])
 def get_category(request):
     categories = CategoryTB.objects.all()
+    search_id = request.query_params.get('id',None)
+    search_name = request.query_params.get('name',None)
+    if search_id:
+        category = categories.filter(id=search_id)
+        categ_serializer = CategorySerializer(category,many = True)
+        return Response(categ_serializer.data)
+    elif search_name:
+        category = categories.filter(name__contains=search_name)
+        categ_serializer = ProductSerializer(category,many = True)
+        return Response(categ_serializer.data)
     categ_serializer = CategorySerializer(categories,many = True)
     return Response(categ_serializer.data)
 
